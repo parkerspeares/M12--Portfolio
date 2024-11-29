72f1dc0da722b3982765918a69b27b39c469d08c
parkerspeares <parker.speares@outlook.com>
2024-11-28 17:27:40 -0800

    error jsx

---
+++ src/components/Error.jsx
@@ -0,0 +1,8 @@
+export default function Error() {
+    return (
+      <div className="error">
+        <h1>Error</h1>
+        <p>Oops! An error has occured trying to access this page.</p>
+      </div>
+    );
+  }
