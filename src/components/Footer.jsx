89c11c7ab33a91e75250fa916570fac0f1c70e7c
parkerspeares <parker.speares@outlook.com>
2024-11-28 17:26:19 -0800

    Footer jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import '../App.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <a href="https://github.com/parkerspeares">
+            <button className="btn button">GitHub</button>
+            </a>
+            <a href="https://www.linkedin.com/in/parker-speares-a24140206/">
+            <button className="btn button">LinkedIn</button>
+            </a>
+        </div>
+    );
+    }
+
+export default Footer;
