5126308ed622b0932ebd3cd42e1c0e4295c98b5a
parkerspeares <parker.speares@outlook.com>
2024-11-28 17:23:38 -0800

    header jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Navigation from './Navigation';
+import '../App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Parker Speares</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
