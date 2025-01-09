707e185d39072f9df4f4bd38256799e501daf066
parkerspeares <parker.speares@outlook.com>
2025-01-09 15:16:33 -0800

    ii

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -10,11 +10,32 @@ export default function Resume() {
                 <h2>Resume</h2>
                 Self-motivated full-stack developer with a passion for crafting scalable, user-centric applications. Eager to leverage expertise in front-end and back-end technologies, database management, and API integration to build innovative solutions that enhance user experience and drive business success. Seeking a challenging role within a dynamic team to contribute to impactful projects with continuous testing and integration, all while working to enhance my technical skills.
 <br></br><br></br><br></br>
-•	Full-stack development:HTML5, CSS Libraries, JavaScript, TypeScript. React.js, Node.js, Rest APIs, MERN Stack, PostgreSQL, GitHub
-•	Data analysis and visualization: Python, MS Excel
-•	CRM tools: Salesforce, JotForm, Bloomerang
-•	Agile project management
-•	Effective communication and team collaboration
+<li>HTML5</li>
+  <li>CSS Libraries</li>
+  <li>JavaScript</li>
+  <li>TypeScript</li>
+  <li>React.js</li>
+  <li>Node.js</li>
+  <li>Rest APIs</li>
+  <li>MERN Stack</li>
+  <li>PostgreSQL</li>
+  <li>GitHub</li>
+  <li>Data analysis and visualization:
+    <ul>
+      <li>Python</li>
+      <li>MS Excel</li>
+    </ul>
+  </li>
+  <li>CRM tools:
+    <ul>
+      <li>Salesforce</li>
+      <li>JotForm</li>
+      <li>Bloomerang</li>
+    </ul>
+  </li>
+  <li>Agile project management</li>
+  <li>Technical Communication</li>
+</ol>
 <br></br><br></br><br></br>
 
 AMERICAN SOCEITY OF CATARACT & REFRACTIVE SURGEONS
@@ -23,7 +44,7 @@ Program Coordinator | April 2022 – April 2024
 •	Managed resources and donations for ASCRS Foundation utilizing tools such as Salesforce, JotForm, and MS Excel.
 •	Facilitated collaboration between patients and physicians 
 •	Facilitated collaboration between physicians and industry leaders to reduce medical waste.
-<br></br>
+<br></br><br></br>
 HEALO5 CORPORATION
 Data Specialist | September 2017 – Feb 2022
 •	Improved decision-making with accurate data analysis and visualizations.
@@ -33,14 +54,14 @@ Data Specialist | September 2017 – Feb 2022
 
 UNIVERSITY OF OREGON FULL STACK BOOTCAMP
 Portland, OR | Certificate of Completion
-<br></br>
+<br></br><br></br>
 UNIVERSITY OF NORTH TEXAS
 Denton, TX | BA International Studies
 <br></br><br></br><br></br>
 Jerome Chenette 
 Senior Web Development Instructor | University of Oregon Full Stack Bootcamp|
 [jerome.chenette@gmail.com] [415 572 8357]
-<br></br>
+<br></br><br></br>
 Nancy Beesley
 CEO | HCB Health| 
 [512-589-2323]
