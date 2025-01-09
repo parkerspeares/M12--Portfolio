73928aefd920a07a3922b6eaa2116b062f108e88
parkerspeares <parker.speares@outlook.com>
2025-01-09 15:10:00 -0800

    resumr

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,15 +7,15 @@ export default function Resume() {
                 <a className="download" href="/resume.png" download="resume.png">Click Here to Download My Resume</a>
             </div>
             <div className="skillsList">
-                <h2>Front End Technologies</h2>
+                <h2>Resume</h2>
                 Self-motivated full-stack developer with a passion for crafting scalable, user-centric applications. Eager to leverage expertise in front-end and back-end technologies, database management, and API integration to build innovative solutions that enhance user experience and drive business success. Seeking a challenging role within a dynamic team to contribute to impactful projects with continuous testing and integration, all while working to enhance my technical skills.
-
+<br></br><br></br><br></br>
 •	Full-stack development:HTML5, CSS Libraries, JavaScript, TypeScript. React.js, Node.js, Rest APIs, MERN Stack, PostgreSQL, GitHub
 •	Data analysis and visualization: Python, MS Excel
 •	CRM tools: Salesforce, JotForm, Bloomerang
 •	Agile project management
 •	Effective communication and team collaboration
-
+<br></br><br></br><br></br>
 
 AMERICAN SOCEITY OF CATARACT & REFRACTIVE SURGEONS
 Program Coordinator | April 2022 – April 2024
@@ -23,28 +23,28 @@ Program Coordinator | April 2022 – April 2024
 •	Managed resources and donations for ASCRS Foundation utilizing tools such as Salesforce, JotForm, and MS Excel.
 •	Facilitated collaboration between patients and physicians 
 •	Facilitated collaboration between physicians and industry leaders to reduce medical waste.
-
+<br></br>
 HEALO5 CORPORATION
 Data Specialist | September 2017 – Feb 2022
 •	Improved decision-making with accurate data analysis and visualizations.
 •	Identified trends in complex datasets to enhance business efficiency.
 •	Developed custom algorithms to optimize data mining processes.
-
+<br></br><br></br><br></br>
 
 UNIVERSITY OF OREGON FULL STACK BOOTCAMP
 Portland, OR | Certificate of Completion
-
+<br></br>
 UNIVERSITY OF NORTH TEXAS
 Denton, TX | BA International Studies
-
+<br></br><br></br><br></br>
 Jerome Chenette 
 Senior Web Development Instructor | University of Oregon Full Stack Bootcamp|
 [jerome.chenette@gmail.com] [415 572 8357]
-
+<br></br>
 Nancy Beesley
 CEO | HCB Health| 
 [512-589-2323]
-
+<br></br><br></br><br></br>
             </div>
         </>
     );
